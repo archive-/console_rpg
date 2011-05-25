a0cabfb1631fad10685afa2f4ec477ec4ca2435d
TJ Koblentz <tj.koblentz@gmail.com>
2011-05-25 13:44:52 -0700

    added a few map related features + time stuff

--- graphics.h
+++ graphics.h
@@ -1,7 +1,19 @@
 #include "libtcod.h"
 
-extern float x, y;
-extern bool entering_text, map_seen[90][120];
+extern float x, y, map_color_scalar[90][120];
+extern bool entering_text;
+
+/* TODO move to elsewhere */
+int get_hour_of_day(void)
+{
+    return 12;
+}
+
+/* TODO move to elsewhere */
+int get_minute_of_day(void)
+{
+    return 20;
+}
 
 void draw_game(TCOD_console_t map_console, TCOD_map_t map, TCOD_console_t chat_console, TCOD_text_t chat)
 {
@@ -16,24 +28,28 @@ void draw_game(TCOD_console_t map_console, TCOD_map_t map, TCOD_console_t chat_c
     else {
         TCOD_text_reset(chat);
     }
-    TCOD_map_compute_fov(map, (int)(x), (int)(y), 10, true, FOV_SHADOW);
+    TCOD_map_compute_fov(map, (int)(x), (int)(y), 10, true, FOV_BASIC);
     for (h = 0; h < 90; ++h) {
         for (w = 0; w < 120; ++w) {
             if (TCOD_map_is_in_fov(map, w, h)) {
-                map_seen[h][w] = true;
+                map_color_scalar[h][w] = 1.f;
                 if (TCOD_map_is_walkable(map, w, h)) {
                     TCOD_console_put_char_ex(map_console, w+1, h+1, ',', TCOD_green, TCOD_dark_green);
                 }
                 else {
                     TCOD_console_put_char_ex(map_console, w+1, h+1, '=', TCOD_orange, TCOD_dark_orange);
                 }
-            }
-            else if (map_seen[h][w]) {
+            } else {
+                map_color_scalar[h][w] -= 0.0005f;
                 if (TCOD_map_is_walkable(map, w, h)) {
-                    TCOD_console_put_char_ex(map_console, w+1, h+1, ',', TCOD_dark_green, TCOD_darker_green);
+                    TCOD_console_put_char_ex(map_console, w+1, h+1, ',',
+                            TCOD_color_multiply_scalar(TCOD_dark_green, map_color_scalar[h][w]), 
+                            TCOD_color_multiply_scalar(TCOD_darker_green, map_color_scalar[h][w]));
                 }
                 else {
-                    TCOD_console_put_char_ex(map_console, w+1, h+1, '=', TCOD_dark_orange, TCOD_darker_orange);
+                    TCOD_console_put_char_ex(map_console, w+1, h+1, '=', 
+                            TCOD_color_multiply_scalar(TCOD_dark_orange, map_color_scalar[h][w]),
+                            TCOD_color_multiply_scalar(TCOD_darker_orange, map_color_scalar[h][w]));
                 }
             }
         }
@@ -42,5 +58,6 @@ void draw_game(TCOD_console_t map_console, TCOD_map_t map, TCOD_console_t chat_c
     TCOD_console_blit(map_console, 0, 0, 122, 92, NULL, 37, 3, 1.f, 1.f);
     TCOD_console_blit(chat_console, 0, 0, 122, 30, NULL, 37, 97, 1.f, 1.f);
     TCOD_console_print_left(NULL, 1, 1, TCOD_BKGND_NONE, "fps: %d", TCOD_sys_get_fps());
+    TCOD_console_print_left(NULL, 20, 1, TCOD_BKGND_NONE, "%2d:%02d", get_hour_of_day(), get_minute_of_day());
     TCOD_console_flush(); /* print screen */
 }
--- main.c
+++ main.c
@@ -8,22 +8,27 @@
 #include "graphics.h"
 
 bool end_game, entering_text;
-float x, y, vx, vy;
-bool w_key, a_key, s_key, d_key, map_seen[90][120];
+float x, y, vx, vy, map_color_scalar[90][120];
+bool w_key, a_key, s_key, d_key;
 
+/* TODO networking */
 void update_game(TCOD_map_t map)
 {
         if (w_key) {
-            if (TCOD_map_is_walkable(map, x, y-1)) y -= vy * TCOD_sys_get_last_frame_length();
+            if (TCOD_map_is_walkable(map, x, y-1))
+                y -= vy * TCOD_sys_get_last_frame_length();
         }
         if (a_key) {
-            if (TCOD_map_is_walkable(map, x-1, y)) x -= vx * TCOD_sys_get_last_frame_length();
+            if (TCOD_map_is_walkable(map, x-1, y))
+                x -= vx * TCOD_sys_get_last_frame_length();
         }
         if (s_key) {
-            if (TCOD_map_is_walkable(map, x, y+1)) y += vy * TCOD_sys_get_last_frame_length();
+            if (TCOD_map_is_walkable(map, x, y+1))
+                y += vy * TCOD_sys_get_last_frame_length();
         }
         if (d_key) {
-            if (TCOD_map_is_walkable(map, x+1, y)) x += vx * TCOD_sys_get_last_frame_length();
+            if (TCOD_map_is_walkable(map, x+1, y))
+                x += vx * TCOD_sys_get_last_frame_length();
         }
 }
 
@@ -32,9 +37,10 @@ int main(void)
     int w, h;
     for (h = 0; h < 90; ++h) {
         for (w = 0; w < 120; ++w) {
-            map_seen[h][w] = false;
+            map_color_scalar[h][w] = 0.f;
         }
     }
+    
     TCOD_console_init_root(160, 128, "console rpg v0.01", true);
     TCOD_console_t map_console = TCOD_console_new(122, 92);
     TCOD_console_t chat_console = TCOD_console_new(122, 30);
@@ -46,8 +52,10 @@ int main(void)
     TCOD_sys_set_fps(60);
     end_game = entering_text = false;
     x = y = 10.f;
-    vx = vy = 6.f;
+    vx = vy = 10.f;
     w_key = a_key = s_key = d_key = false;
+    
+    /* TODO generate map properties differently -- load from file, etc. */
     for (h = 0; h < 90; ++h) {
         for (w = 0; w < 120; ++w) {
             if (h == 0 || h == 89 || w == 0 || w == 99 || (w == 24 && h != 14 && h != 15)) {
@@ -58,10 +66,13 @@ int main(void)
             }
         }
     }
+
+    /* TODO game loop */
     while (!end_game && !TCOD_console_is_window_closed()) {
         handle_input(chat);
         update_game(map);
         draw_game(map_console, map, chat_console, chat);
     }
+
     return EXIT_SUCCESS;
 }
